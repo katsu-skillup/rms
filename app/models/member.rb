d3cd6cfdfc14bb7b3c65cd559e9e795de389cd82
katsu-skillup <seraphyk@gmail.com>
2021-09-06 20:59:21 +0900

    アソシエーション及びバリデーションの設定

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,16 @@
 class Member < ApplicationRecord
+belongs_to :user
+has_many :meetings
+
+with_options presence: true do
+  validates :member_name
+  validates :organization
+  validates :work_info
+  validates :valuation_id
+  validates :communication
+  validates :leadership
+  validates :skill
+  validates :motivation
+end 
+
 end
