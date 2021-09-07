a4016f3980cc857a56e2ad46f9fedfecfc564eee
katsu-skillup <seraphyk@gmail.com>
2021-09-07 22:36:11 +0900

    メンバー登録及びメンバー一覧表示機能実装

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,28 @@
 class MembersController < ApplicationController
+
+def index
+  members = Member.all
+  @members = current_user.members.order(valuation_id:"ASC")
+end
+
+def new
+  @member = Member.new
+end
+
+def create
+  @member = Member.new(member_params)
+    if @member.save
+      redirect_to root_path
+    else
+      render :new
+    end
+end
+
+private
+
+def member_params
+  params.require(:member).permit(:image, :member_name, :organization, :position, :work_info, :valuation_id, :communication, :leadership, :skill, :motivation).merge(user_id: current_user.id)
+end
+
+
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,16 +1,21 @@
 class Member < ApplicationRecord
-belongs_to :user
-has_many :meetings
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :valuation
 
-with_options presence: true do
-  validates :member_name
-  validates :organization
-  validates :work_info
-  validates :valuation_id
-  validates :communication
-  validates :leadership
-  validates :skill
-  validates :motivation
-end 
+  belongs_to :user
+  has_many :meetings
+  has_one_attached :image
 
+  with_options presence: true do
+    validates :member_name
+    validates :organization
+    validates :work_info
+    validates :valuation_id
+    validates :communication
+    validates :leadership
+    validates :skill
+    validates :motivation
+  end 
+
+  validates :valuation_id, numericality: { other_than: 1 } 
 end
