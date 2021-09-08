077dd6ec27b71d5cb6ae8d513c46847240582b1c
katsu-skillup <seraphyk@gmail.com>
2021-09-09 01:23:32 +0900

    面談記録機能の実装

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,2 +1,22 @@
 class MeetingsController < ApplicationController
+  before_action :authenticate_user!
+  def new
+    @meeting = Meeting.new
+  end
+
+  def create
+    @meeting = Meeting.new(meeting_params)
+    if @meeting.save
+      redirect_to member_path(params[:member_id])
+    else
+       render :new
+    end
+  end
+
+private
+  
+  def meeting_params
+    params.require(:meeting).permit(:meeting_date, :meeting_info).merge(member_id: params[:member_id] ,user_id: current_user.id)
+  end
+
 end
--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -20,7 +20,27 @@ def create
 end
 
  def show
+  @member = Member.find(params[:id])
  end
+
+ def edit
+  @member = Member.find(params[:id])
+end
+
+def update
+  @member = Member.find(params[:id])
+  if @member.update(member_params)
+    redirect_to member_path(params[:id])
+   else
+     render :edit
+   end
+  end
+
+def destroy
+  @member = Member.find(params[:id])
+  @member.destroy
+  redirect_to members_path
+end
 
 private
 
