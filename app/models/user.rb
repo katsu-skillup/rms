466162648989e8a765a166063835a0c40c2bf2c0
katsu-skillup <seraphyk@gmail.com>
2021-09-19 20:10:24 +0900

    SNS認証実証

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,30 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
-validates :name, presence: true
+  validates :name, presence: true
 
-PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
-validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 
-has_many :members
-has_one :meeting
+  has_many :members
+  has_one :meeting
+  has_many :sns_credentials
 
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    # sns認証したことがあればアソシエーションで取得
+    # 無ければnameとemailでユーザー検索して取得orビルド（保存はしない）
+    user = User.where(name: auth.info.name).where(email: auth.info.email).first_or_initialize(
+      name: auth.info.name,
+      email: auth.info.email
+    )
+    # userが登録済みであるか判断
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
+  end
 end
